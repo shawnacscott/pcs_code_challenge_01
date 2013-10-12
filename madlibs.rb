9e58bc9454f1bf5da6f4942814157c6a9ff153ef
Shawna C Scott <shawnatyger@gmail.com>
2013-10-12 16:27:27 -0700

    intermediate playing around

--- madlibs.rb
+++ madlibs.rb
@@ -1,28 +1,78 @@
+# This is increment 4
 
-# This is increment 3
+class Madlibs
+	def self.prompt
+		puts "Please enter a noun or noun phrase:"
+		noun = gets.strip.downcase
 
-puts "Please enter a noun or noun phrase:"
-noun = gets.strip.downcase
+		puts "Please enter an adjective or adjective phrase:"
+		adjective = gets.strip.downcase
 
-puts "Please enter an adjective or adjective phrase:"
-adjective = gets.strip.downcase
+		puts "Please enter a present tense verb or verb phrase:"
+		verb = gets.strip.downcase
 
-puts "Please enter a present tense verb or verb phrase:"
-verb = gets.strip.downcase
+		puts "Please enter an adverb or adverb phrase:"
+		adverb = gets.strip.downcase
 
-puts "Please enter an adverb or adverb phrase:"
-adverb = gets.strip.downcase
+		puts "Please enter an object or object phrase:"
+		object = gets.strip.downcase
 
-puts "Please enter an object or object phrase:"
-object = gets.strip.downcase
+		madlibs = {"noun" => noun, "adjective" => adjective, "verb" => verb, "adverb" => adverb, "object" => object}
+	end
+end
 
-madlibs = {"noun" => noun, "adjective" => adjective, "verb" => verb, "adverb" => adverb, "object" => object}
+madlibs = Madlibs.prompt
 
-puts madlibs
+template_array = [
+			SentenceTemplate.new("The adjective noun suddenly and adverb had to verb the object.")
+		]
 
-array = ["The #{madlibs['adjective']} #{madlibs['noun']} suddenly and #{madlibs['adverb']} had to #{madlibs['verb']} the #{madlibs['object']}", "The #{madlibs['noun']} jumped over the #{madlibs['object']}", "The #{madlibs['noun']} says 'let's #{madlibs['adverb']} #{madlibs['verb']} to the #{madlibs['adjective']} #{madlibs['object']}!'"]
+class SentenceTemplate
 
-puts array[0]
-puts array[1]
-puts array[2]
+	def initialize(template_string)
+		insert
+		puts "reached initialize"
+	end
+
+	def insert
+		template_output = template_array.each.gsub(/noun/, madlibs)
+		puts "reached insert"
+	end
+
+end
+
+
+puts template_output.each
+
+
+# class SentenceTemplates
+# 	def template_01
+# 		@template_01 = "Test Output"
+# 	end 
+# 	def self.output
+# 		puts @template_01
+# 	end
+# end
+
+# output = SentenceTemplates.new
+# output.template_01.output
+
+# #class SentenceTemplates
+# #	def self.sentence_01
+# #		@sentence_01 = ["The #{madlibs['adjective']} #{madlibs['noun']} suddenly and #{madlibs['adverb']} had to #{madlibs['verb']} the #{madlibs['object']}"
+# #	end
+# #	def insert
+# #
+# #	end
+# #end
+
+
+
+# # array = [
+# # 	"The #{madlibs['adjective']} #{madlibs['noun']} suddenly and #{madlibs['adverb']} had to #{madlibs['verb']} the #{madlibs['object']}", 
+# # 	"The #{madlibs['noun']} jumped over the #{madlibs['object']}", 
+# # 	"The #{madlibs['noun']} says 'let's #{madlibs['adverb']} #{madlibs['verb']} to the #{madlibs['adjective']} #{madlibs['object']}!'"
+# # ]
+
+# # puts array.each { |x| puts x }
 
